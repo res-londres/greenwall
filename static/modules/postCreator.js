3540d53263bf2f5c6e547cc2530cf63c20f3f45a
res-londres <jimuel.londres@gmail.com>
2026-09-09 15:15:20 +0800

    refactor(page-creator): add page creator events handler

--- static/modules/postCreator.js
+++ static/modules/postCreator.js
@@ -1,19 +1,38 @@
-document.addEventListener('click', function(event) {
-    const actionElement = event.target.closest('[data-action');
-    if (actionElement) {
-        const action = actionElement.dataset.action;
-        if (action === 'openPostCreatorModal') {
-            openPostCreatorModal();
-        } else if (action == 'closePostCreatorModal') {
-            closePostCreatorModal();
+function handlePostCreatorEvents() {
+    const postCreatorModal = document.getElementById('post-creator-modal');
+
+    // POST CREATOR //
+    document.querySelectorAll('.post-creator').forEach(function(postCreator) {
+        postCreator.addEventListener('click', function(event) {
+            const actionElement = event.target.closest('[data-action');
+            if (actionElement) {
+                const action = actionElement.dataset.action;
+                if (action === 'openPostCreatorModal') {
+                    openPostCreatorModal(postCreatorModal);
+                }
+            }
+        });
+    });
+
+    // POST CREATOR MODAL //
+    postCreatorModal.addEventListener('click', function(event) {
+        const actionElement = event.target.closest('[data-action]');
+        if (actionElement) {
+            const action = actionElement.dataset.action;
+            if (action === 'closePostCreatorModal') {
+                closePostCreatorModal(postCreatorModal);
+            }
         }
-    }
-});
-
-function openPostCreatorModal() {
-    document.getElementById('post-creator-modal').style.display = 'flex';
+    });
 }
 
-function closePostCreatorModal() {
-    document.getElementById('post-creator-modal').style.display = 'none';
+function openPostCreatorModal(postCreatorModal) {
+    postCreatorModal.style.display = 'flex';
 }
+
+function closePostCreatorModal(postCreatorModal) {
+    postCreatorModal.style.display = 'none';
+}
+
+// INIT //
+handlePostCreatorEvents();
