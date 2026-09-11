3d969e3d3e38b1af0543fe2bf631dc35a7d8c10b
res-londres <jimuel.londres@gmail.com>
2026-09-12 01:23:57 +0800

    feat(html-creator): add html creator for post modal

--- static/modules/HTMLCreator.js
+++ static/modules/HTMLCreator.js
@@ -10,7 +10,7 @@ export function createEmptyWallHTML() {
 }
 
 export function createPostHTML(post) {
-    const postID = post.id;
+    const postID = post.post_id;
     const postAttribution = post.attribution;
     const postSubject = escapeHTML(post.subject);
     const postContentPreview = escapeHTML(post.content.length > 500 ? post.content.slice(0, 497) + '...' : post.content);
@@ -35,16 +35,70 @@ export function createPostHTML(post) {
             <div class="text-[0.95rem] leading-6 mb-3 wrap-break-word">${postContentPreview}</div>
             <div class="items-center border-t border-solid border-muted flex gap-6 pt-2">
                 <button class="post-action" data-action="likePost" data-postid="${postID}">
-                    <span class="">
+                    <span>
                         <span class="text-2xl icon-[ant-design--heart-outlined]"></span>
                     </span>
-                    <span class="">${postLikeCount}</span>
+                    <span>${postLikeCount}</span>
                 </button>
                 <button class="post-action" data-action="openPostModal" data-postid="${postID}">
-                    <span class="">
+                    <span>
                         <span class="text-2xl icon-[ant-design--comment-outlined]"></span>
                     </span>
-                    <span class="">${commentCount}</span>
+                    <span>${commentCount}</span>
+                </button>
+            </div>
+        </div>
+    `;
+}
+
+export function createPostModalHTML(post) {
+    const postID = post.post_id;
+    const postAttribution = post.attribution;
+    const postSubject = escapeHTML(post.subject);
+    const postContent = escapeHTML(post.content);
+    const postTime = 'just now'; // TEMP
+    const postLikeCount = '0';   // TEMP
+    const commentCount = '0';
+
+    return `
+        <div class="bg-white text-primary border-b-2 border-solid border-muted mb-4 pb-2">
+            <div class="items-center flex justify-between mb-2">
+                <span class="font-bold">${postAttribution}</span>
+                <span class="text-gray-500 text-[0.8rem]">${postTime}</span>
+            </div>
+            <div class="font-bold leading-6 mb-3 wrap-break-word">${postSubject}</div>
+            <div class="text-[0.95rem] leading-6 mb-3 wrap-break-word">${postContent}</div>
+            <div class="border-t border-solid border-muted flex gap-6 mt-3 pt-3">
+                <button class="post-action" data-action="likePost" data-postid="${postID}">
+                    <span>
+                        <span class="text-2xl icon-[ant-design--heart-outlined]"></span>
+                    </span>
+                    <span>${postLikeCount}</span>
+                </button>
+                <button class="post-action" data-action="closePostModal">
+                    <span>
+                        <span class="text-2xl icon-[ant-design--comment-outlined]"></span>
+                    </span>
+                    <span>${commentCount}</span>
+                </button>
+            </div>
+        </div>
+
+        <div class="text-primary">
+            <div class="font-bold mb-3">Comments (${commentCount})</div>
+            <div id="post-modal-comment-list">
+                <!-- TODO: commentsHTML later -->
+            </div>
+            <div class="bg-white border-t border-solid border-muted flex gap-2 mt-3 py-4 sticky bottom-0 z-30">
+                <input 
+                    type="text" 
+                    id="post-modal-comment"
+                    class="bg-muted border-2 border-solid border-muted rounded-4xl flex-1 font-[inherit] p-[0.5rem_0.75rem] focus:outline-none text-[0.9rem]"
+                    placeholder="Write a comment.."
+                    maxlength="1000"
+                >
+                <button id="comment-button" class="post-action" data-action="submitComment" data-postid="${postID}">
+                    <span class="text-2xl icon-[griddy-icons--send]"></span>
                 </button>
             </div>
         </div>
