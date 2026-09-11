4ab68ed9ded6a7b0d89c3a429198de86ad0de314
res-londres <jimuel.londres@gmail.com>
2026-09-12 01:28:35 +0800

    feat(rendering): add renderPostModal; add subscription to renderPostModal when post modal opens

--- static/modules/postRenderer.js
+++ static/modules/postRenderer.js
@@ -1,17 +1,19 @@
 import * as bus from './eventBus.js';
 import * as HTMLCreator from './HTMLCreator.js';
+import * as postManager from './postManager.js';
 import { getCurrentWall } from './wallManager.js';
-import { getGlobalPosts } from './postManager.js';
 
 // INIT //
 export function init() {
     renderGlobalPosts();
+    // TODO: postAdded => addPost
     bus.on('postManager:postAdded', renderGlobalPosts);
+    bus.on('post:openPostModal', renderPostModal);
 }
 
 function renderGlobalPosts() {
     const currentWall = getCurrentWall();
-    const globalPosts = getGlobalPosts();
+    const globalPosts = postManager.getGlobalPosts();
     if (globalPosts.length === 0) {
         currentWall.innerHTML = HTMLCreator.createEmptyWallHTML();
         return;
@@ -22,4 +24,10 @@ function renderGlobalPosts() {
         html += HTMLCreator.createPostHTML(post);
     });
     currentWall.innerHTML += html;
+}
+
+function renderPostModal() {
+    const postModalContent = document.getElementById('post-modal-content');
+    const post = postManager.getCurrentPost();
+    postModalContent.innerHTML = HTMLCreator.createPostModalHTML(post);
 }
