b7922c3e69182629697851d2965fe9f5b3edbf13
res-londres <jimuel.londres@gmail.com>
2026-09-12 15:54:07 +0800

    feat(post-manager): add post comments manager

--- static/modules/postManager.js
+++ static/modules/postManager.js
@@ -1,10 +1,15 @@
 import * as bus from './eventBus.js';
 
-const globalPosts = {};      // {post_id: {post}, post_id: {another_post}}
+const globalPosts = {};     // {post_id: {post}, post_id: {another_post}}
+const postComments = {};    // {post_id: {comment_id: {comment}, comment_id: {another_comment}}, post_id: {comments}}
 let currentPostID = null;
 
 export function setCurrentPostID(newPostID) {
     currentPostID = newPostID;
+}
+
+export function getCurrentPostId() {
+    return currentPostID;
 }
 
 // deprecated
@@ -20,8 +25,21 @@ export function getCurrentPost() {
     return structuredClone(globalPosts[currentPostID]);
 }
 
+export function getCommentsList() {
+    return Object.values(postComments[currentPostID]);
+}
+
 export function addPost(post) {
     const postID = post.post_id;
     globalPosts[postID] = post;
     bus.emit('postManager:addPost');
+}
+
+export function addComment(comment) {
+    const commentID = comment.comment_id;
+    if (!(currentPostID in postComments)) {
+        postComments[currentPostID] = {};
+    }
+    postComments[currentPostID][commentID] = comment;
+    bus.emit('postManager:addComment');
 }
