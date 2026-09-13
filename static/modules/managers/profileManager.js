214f44e8c407f9eb6189d94b8f8522579a93fc65
res-londres <jimuel.londres@gmail.com>
2026-09-13 20:43:24 +0800

    feat(profile-manager): add profile manager
    - manage user profile and public accounts

---
+++ static/modules/managers/profileManager.js
@@ -0,0 +1,43 @@
+// ALIAS: publicAccount => account
+
+const userProfile = {
+    user_id: 'user_id',    
+    username: 'username'   
+}
+
+const publicAccounts = {
+    account_id1: {
+        account_id: 'account_id1',
+        display_name: 'account1'
+    },
+    account_id2: {
+        account_id: 'account_id2',
+        display_name: 'account2'
+    },
+    account_id3: {
+        account_id: 'account_id3',
+        display_name: 'account3'
+    }
+}
+
+let currentAccountID = 'account_id1';
+
+export function getUserProfile() {
+    return structuredClone(userProfile);
+}
+
+export function getPublicAccounts() {
+    return structuredClone(publicAccounts);
+}
+
+export function getCurrentAccount() {
+    return structuredClone(publicAccounts[currentAccountID]);
+}
+
+export function setCurrentAccountID(newAccountID) {
+    currentAccountID = newAccountID;
+}
+
+export function getCurrentAccountID() {
+    return currentAccountID;
+}
