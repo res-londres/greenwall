ccb6253f722af508da73def1ea35c4293a613e60
res-londres <jimuel.londres@gmail.com>
2026-09-20 23:31:19 +0800

    feat(liking): store post likes by account
    - previously, post likes are stored per user
    - so if a user likes a post in one account, it stays liked in another account
    - now, each account's post likes are isolated

--- static/modules/managers/likeManager.js
+++ static/modules/managers/likeManager.js
@@ -1,19 +1,29 @@
 import { getCurrentPostID } from './postManager.js';
+import { getCurrentAccountID } from './profileManager.js';
 
-const likedPosts = {}     // {post_id: true, post_id: true}
+const likedPostsByAccount = {}     // {account_id: {post_id: true, post_id: true}}
 const likedCommentsByPost = {}     // {post_id: {comment_id: true, comment_id: true}, post_id: {..}}
 
 // POST LIKES //
 export function isPostLiked(postID) {
-    return postID in likedPosts;
+    const accountID = getCurrentAccountID();
+    return accountID in likedPostsByAccount && postID in likedPostsByAccount[accountID];
 }
 
 export function addLikedPost(postID) {
-    likedPosts[postID] = true;
+    const accountID = getCurrentAccountID();
+    if (!(accountID in likedPostsByAccount)) {
+        likedPostsByAccount[accountID] = {};
+    }
+    likedPostsByAccount[accountID][postID] = true;
 }
 
 export function removeLikedPost(postID) {
-    delete likedPosts[postID];
+    const accountID = getCurrentAccountID();
+    if (!(accountID in likedPostsByAccount)) {
+        return;
+    }
+    delete likedPostsByAccount[accountID][postID];
 }
 
 // COMMENT LIKES //
